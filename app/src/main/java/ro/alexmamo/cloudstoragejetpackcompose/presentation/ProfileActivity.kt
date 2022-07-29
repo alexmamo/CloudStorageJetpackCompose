e501a8b45bd75eb5ae19cec1c6fa8bb84ecfac18
Alex Mamo <alexmamo@gmail.com>
2022-07-29 18:54:22 +0300

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/cloudstoragejetpackcompose/presentation/ProfileActivity.kt
+++ app/src/main/java/ro/alexmamo/cloudstoragejetpackcompose/presentation/ProfileActivity.kt
@@ -1,13 +1,13 @@
 package ro.alexmamo.cloudstoragejetpackcompose.presentation
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import ro.alexmamo.cloudstoragejetpackcompose.presentation.components.ProfileImageScreen
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
